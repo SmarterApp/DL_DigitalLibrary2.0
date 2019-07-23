db50e7277c259146320c0ce206815c14fafdf2ac
Brandon Savage <bsavage@fairwaytech.com>
2019-07-23 16:39:41 -0700

    Initial template and styles for the resource outline.  Added new custom icons.

--- webapp/src/app/resource/outline/outline.component.spec.ts
+++ webapp/src/app/resource/outline/outline.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OutlineComponent } from './outline.component';
+import { SbdlCommonModule } from 'src/app/common/common.module';
 
 describe('OutlineComponent', () => {
   let component: OutlineComponent;
@@ -8,6 +9,7 @@ describe('OutlineComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ SbdlCommonModule ], 
       declarations: [ OutlineComponent ]
     })
     .compileComponents();
--- webapp/src/app/resource/outline/outline.component.ts
+++ webapp/src/app/resource/outline/outline.component.ts
@@ -13,3 +13,4 @@ export class OutlineComponent implements OnInit {
   }
 
 }
+
