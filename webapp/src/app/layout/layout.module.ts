171ec4c6c5002e952fa27b6b9f7651b0bf98cdb0
Brandon Savage <bsavage@fairwaytech.com>
2019-07-25 09:11:56 -0700

    Resolved issue with the resource-type-strategy component not reloading the data on param changes (Now subscribing to changes rather than ngOnInit to be called on param changes)

--- webapp/src/app/layout/layout.module.ts
+++ webapp/src/app/layout/layout.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AppContainerComponent } from './app-container/app-container.component';
 import { HeaderComponent } from './header/header.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
