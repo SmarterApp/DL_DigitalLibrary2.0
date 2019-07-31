6ce710ace505233199413ea19cb69aa38b0827ef
Brandon Savage <bsavage@fairwaytech.com>
2019-07-31 15:41:55 -0700

    Added a popover component.  Added shared functionality which opens a share popover allowing the user to copy the url to the clipboard.

--- webapp/src/app/common/common.module.ts
+++ webapp/src/app/common/common.module.ts
@@ -5,14 +5,17 @@ import { ButtonComponent } from './controls/button/button.component';
 import { ButtonIconComponent } from './controls/button-icon/button-icon.component';
 import { TooltipComponent } from './controls/tooltip/tooltip.component';
 import { TooltipModule } from 'ng2-tooltip-directive';
+import { PopoverComponent } from './controls/popover/popover.component';
+import { CommonModule } from '@angular/common';
 @NgModule({
-    imports: [ TooltipModule ],
+    imports: [ TooltipModule, CommonModule ],
     declarations: [
         ButtonComponent,
         ButtonIconComponent,
         IconComponent,
         SvgDefsComponent,
-        TooltipComponent
+        TooltipComponent,
+        PopoverComponent
     ],
     exports: [
         ButtonComponent,
@@ -20,7 +23,8 @@ import { TooltipModule } from 'ng2-tooltip-directive';
         IconComponent,
         SvgDefsComponent,
         TooltipComponent
-    ]
+    ],
+    entryComponents: [ PopoverComponent ]
 })
 export class SbdlCommonModule {
 }
