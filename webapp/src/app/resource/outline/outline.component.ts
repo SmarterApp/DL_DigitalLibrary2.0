dcc17ac8cfbd78d9bab3ed13cfc46d80b4ad622d
Brandon Savage <bsavage@fairwaytech.com>
2019-08-02 16:50:42 -0700

    Initial pass using an intersection observer to notify when elements have scrolled on the page.

--- webapp/src/app/resource/outline/outline.component.ts
+++ webapp/src/app/resource/outline/outline.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { ResourceModel } from 'src/app/data/resource/model/resource.model';
 import { ScrollableElements } from './scrollable-elements.model';
 
@@ -7,7 +7,8 @@ import { ScrollableElements } from './scrollable-elements.model';
   templateUrl: './outline.component.html',
   styleUrls: ['./outline.component.scss']
 })
-export class OutlineComponent implements OnInit {
+export class OutlineComponent implements OnInit, OnDestroy {
+
   @Input()
   model: ResourceModel;
 
@@ -15,15 +16,67 @@ export class OutlineComponent implements OnInit {
   readingMode: boolean;
 
   @Input()
-  scrollableElements: ScrollableElements = <ScrollableElements>{};
+  set scrollableElements(value: ScrollableElements) {
+    this._scrollableElements = value;
+    if(this._scrollableElements) {
+      this.wireScrollableObservers();
+    }
+  }
+  get scrollableElements() {
+    return this._scrollableElements;
+  }
+
+  getStartedScrolled = false;
+  differentiationScrolled = false;
+  formativeScrolled = false;
+
+  private _scrollableElements: ScrollableElements = <ScrollableElements>{};
+  private observers: IntersectionObserver[] =[];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+    for(let observer of this.observers) {
+      observer.disconnect();
+    }
+  }
+
   scrollToElement(element: Element): void {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    this.resetScrolled();
+  }
+
+  private wireScrollableObservers() {
+    this.wireIntersectionObserver(this._scrollableElements.getStarted, scrolled => this.getStartedScrolled = scrolled);
+    this.wireIntersectionObserver(this._scrollableElements.differentiation, scrolled => this.differentiationScrolled = scrolled);
+    this.wireIntersectionObserver(this._scrollableElements.formativeAssessmentProcess, scrolled => this.formativeScrolled = scrolled)
+  }
+
+  private wireIntersectionObserver(element: any, setScrolled) {
+    var observer = new IntersectionObserver((entries) => {
+      // isIntersecting is true when element and viewport are overlapping
+      // isIntersecting is false when element and viewport don't overlap
+      console.log('observing', element);
+      if(entries[0].isIntersecting === true) {
+        this.resetScrolled();
+        setScrolled(true);
+        console.log('Element has just become visible in screen');
+      } else {
+        setScrolled(false);
+      }
+    }, { threshold: [0] });
+
+    observer.observe(element);
+    this.observers.push(observer);
+  }
+
+  private resetScrolled() {
+    this.getStartedScrolled = false;
+    this.differentiationScrolled = false;
+    this.formativeScrolled = false;
   }
 }
 
