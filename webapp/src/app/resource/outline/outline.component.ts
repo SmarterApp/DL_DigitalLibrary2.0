0c841d65eea6eb7d78980d906f9f071856ba8ae8
Brandon Savage <bsavage@fairwaytech.com>
2019-08-05 13:06:28 -0700

    Continued tweaking of the location indicator.  This impl gets a little closer, it'll remove the focus of the clicked button on scroll, and stores what the "nextToScroll" will be when there are more than 1 scrollable element in the viewport.

--- webapp/src/app/resource/outline/outline.component.ts
+++ webapp/src/app/resource/outline/outline.component.ts
@@ -19,16 +19,29 @@ export class OutlineComponent implements OnInit, OnDestroy {
   set scrollableElements(value: ScrollableElements) {
     this._scrollableElements = value;
     if(this._scrollableElements) {
-      this.wireScrollableObservers();
+      // Experimental.  This functionality seems fine at first, but after a few clicks it starts to become a little out of synch and wonky.
+      // Uncomment the following line to try:
+      // this.wireScrollableObservers();
     }
   }
   get scrollableElements() {
     return this._scrollableElements;
   }
 
-  getStartedScrolled = false;
-  differentiationScrolled = false;
-  formativeScrolled = false;
+  get getStartedScrolled() {
+    return this.currentScrolled && this.currentScrolled.section  === OutlineSection.GetStarted;
+  }
+
+  get differentiationScrolled() {
+    return this.currentScrolled && this.currentScrolled.section === OutlineSection.Differentiation;
+  }
+
+  get formativeScrolled() {
+    return this.currentScrolled && this.currentScrolled.section  === OutlineSection.FormativeAssessmentProcess;
+  }
+
+  currentScrolled = null;
+  nextToScroll = null;
 
   private _scrollableElements: ScrollableElements = <ScrollableElements>{};
   private observers: IntersectionObserver[] =[];
@@ -46,37 +59,54 @@ export class OutlineComponent implements OnInit, OnDestroy {
 
   scrollToElement(element: Element): void {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-    this.resetScrolled();
   }
 
+  // Experimental.  This functionality seems fine at first, but after a few clicks it starts to become a little out of synch and wonky.
   private wireScrollableObservers() {
-    this.wireIntersectionObserver(this._scrollableElements.getStarted, scrolled => this.getStartedScrolled = scrolled);
-    this.wireIntersectionObserver(this._scrollableElements.differentiation, scrolled => this.differentiationScrolled = scrolled);
-    this.wireIntersectionObserver(this._scrollableElements.formativeAssessmentProcess, scrolled => this.formativeScrolled = scrolled)
+    this.wireIntersectionObserver(this._scrollableElements.getStarted, OutlineSection.GetStarted);
+    this.wireIntersectionObserver(this._scrollableElements.differentiation, OutlineSection.Differentiation);
+    this.wireIntersectionObserver(this._scrollableElements.formativeAssessmentProcess, OutlineSection.FormativeAssessmentProcess);
   }
 
-  private wireIntersectionObserver(element: any, setScrolled) {
+  private wireIntersectionObserver(element: any, section: OutlineSection) {
     var observer = new IntersectionObserver((entries) => {
+      if(document.activeElement) {
+        document.activeElement.blur();
+      }
       // isIntersecting is true when element and viewport are overlapping
       // isIntersecting is false when element and viewport don't overlap
-      console.log('observing', element);
-      if(entries[0].isIntersecting === true) {
-        this.resetScrolled();
-        setScrolled(true);
-        console.log('Element has just become visible in screen');
+      const entry = entries[0];
+      console.log('observing', element.innerText);
+      if(entry.isIntersecting === true) {
+        console.log('Element ' + entry.target.innerText + ' has just become visible in screen at ', entry);
+        if(this.currentScrolled == null || entry.target.offsetTop < this.currentScrolled.entry.target.offsetTop) {
+          if(this.currentScrolled != null) {
+            console.log('Element ' + entry.target.innerText + ' had a lower position than current ', this.currentScrolled.entry);
+          }
+          this.currentScrolled = { section: section, entry: entry };
+        } else {
+          this.nextToScroll =  { section: section, entry: entry };
+        }
+        
       } else {
-        setScrolled(false);
+        console.log('Element ' + entry.target.innerText + ' is no longer visible in screen.');
+
+        if(this.nextToScroll != null) {
+          this.currentScrolled = this.nextToScroll;
+          this.nextToScroll = null;
+        }
       }
     }, { threshold: [0] });
 
     observer.observe(element);
     this.observers.push(observer);
   }
-
-  private resetScrolled() {
-    this.getStartedScrolled = false;
-    this.differentiationScrolled = false;
-    this.formativeScrolled = false;
-  }
 }
 
+enum OutlineSection {
+  GetStarted,
+  Steps,
+  Attachments,
+  Differentiation, 
+  FormativeAssessmentProcess
+}
