c0b36914d8d788e7d928a3bac21a502709aafacc
Jonathan Bernard <jbernard@fairwaytech.com>
2019-08-08 03:27:38 -0500

    Failed to bash properly.

--- webapp/codebuild/deploy.sh
+++ webapp/codebuild/deploy.sh
@@ -18,15 +18,18 @@ echo "  DL_TARGET_ENV = ${DL_TARGET_ENV}"
 # 1. A pull-request is merged into develop. In this case CodeBuild is triggered
 #    by the GitHub webhook. If it is a PR merge into develop, we know we're
 #    deploying to dev.
-if [ $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_MERGED' -a
-     $CODEBUILD_WEBHOOK_BASE_REF == 'develop' ]; then
+if [[ -n $CODEBUILD_WEBHOOK_EVENT && \
+      -n $CODEBUILD_WEBHOOK_BASE_REF && \
+      $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_MERGED' && \
+      $CODEBUILD_WEBHOOK_BASE_REF == 'develop' ]]; then
   target_env="dev"
 
 # 2. A pull request is create, updated, or re-opened. In this case we only want
 #    CodeBuild to build the project based on the PR. We don't want to deploy.
-elif [ $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_CREATED' -o
-       $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_UPDATED' -o
-       $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_REOPENED' ]; then
+elif [[ -n $CODEBUILD_WEBHOOK_EVENT && \
+        ( $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_CREATED' || \
+          $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_UPDATED' || \
+          $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_REOPENED' ) ]]; then
 
   echo "This is a PR build, no deployment required."
   exit 0
@@ -34,8 +37,8 @@ elif [ $CODEBUILD_WEBHOOK_EVENT == 'PULL_REQUEST_CREATED' -o
 # 3. A build has been manually triggered (deploy to QA, UAT, Staging, or PROD).
 #    In this case there won't be any information about the webhook, but we do
 #    expect to see the DL_TARGET_ENV variable set
-elif [ -z $CODEBUILD_WEBHOOK_EVENT -a
-       -n $DL_TARGET_ENV ]; then
+elif [[ -z $CODEBUILD_WEBHOOK_EVENT && \
+        -n $DL_TARGET_ENV ]]; then
   target_env="$DL_TARGET_ENV"
 
 # All build invocations should fit one of the categories above.
@@ -52,7 +55,12 @@ fi
 # Now we can perform the actual deploy.
 target_domain="${target_env}.webapp.dl.smarterbalanced.org"
 echo "Deploy target is ${target_env}. Deploying to ${target_domain}."
-aws s3 sync webapp/dist/sb-digital-library "${target_domain}"
+aws s3 sync ./dist/sb-digital-library/ "s3://${target_domain}"
+
+if [[ $? -ne 0 ]]; then
+  >&2 echo "Failed to sync dist contents to target S3 bucket."
+  exit 2
+fi
 
 # The following uses the --query feature of AWS CLI to directly extract the ID of
 # the distribution we're targetting. The value is a JMESPath expression which
@@ -92,13 +100,24 @@ cloudfront_distribution_id=$(\
     --query "DistributionList.Items[?starts_with(Origins.Items[0].DomainName, '${target_domain}')].Id | [0]" \
   | sed -e 's/^"//' -e 's/"$//'
 )
+
+if [[ -z "${cloudfront_distribution_id}" ]]; then
+  >&2 echo "Unable to find CloudFront distribution for domain ${target_domain}."
+  exit 3
+fi
+
 echo "Found distribution ID ${cloudfront_distribution_id}."
 
 echo "Invalidating the CloudFront cache for ${target_env}."
-invalidation_id=aws cloudfront create-invalidation \
-  --distribution-id "${cloudfront_distribution_id} \
-  --paths '/index.html' \\
-  --query 'Invalidation.id'
+invalidation_id=$(aws cloudfront create-invalidation \
+  --query 'Invalidation.Id' \
+  --distribution-id "${cloudfront_distribution_id}" \
+  --paths '/index.html')
 
-echo "Successfully created invalidation ${invalidation_id}."
+if [[ $? -ne 0 || -z "${invalidation_id}" ]]; then
+  >&2 echo "Unable to create the CloudFront invalidation."
+else
+  echo "Successfully created invalidation ${invalidation_id}."
+fi
+
 echo "Done."
