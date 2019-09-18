ebac93b2a7b5720fa58b38c1f399b05dc3561947
Brandon Savage <bsavage@fairwaytech.com>
2019-09-18 16:19:05 -0700

    Initial setup of search module on the home page component.

--- webapp/src/app/home/home.module.ts
+++ webapp/src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { SearchModule } from '../search/search.module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     RouterModule,
+    SearchModule,
     CommonModule
   ]
 })
