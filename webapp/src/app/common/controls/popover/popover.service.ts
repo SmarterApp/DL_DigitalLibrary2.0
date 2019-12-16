4dfc70658ff9a22ca0f23c9bf4ae36db1facd7b5
Jonathan Bernard <jbernard@fairwaytech.com>
2019-12-15 23:53:24 -0600

    Makes tooltip components more accessible.
    * Make both the tooltip open and close elements buttons so they are
      included in the tab order of the page and more naturally presented to
      screen readers and other accessibility tools.
    * Style tweaks to tooltips.
    * Add an indicator in the tooltip link title that links open in new
      pages.
    * Popover components are now given focus when they are created.

--- webapp/src/app/common/controls/popover/popover.service.ts
+++ webapp/src/app/common/controls/popover/popover.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, ComponentFactoryResolver, Injector, ViewContainerRef, ReflectiveInjector, ApplicationRef, EmbeddedViewRef } from '@angular/core';
+import { Injectable, ComponentFactoryResolver, Injector, ViewContainerRef,
+  ReflectiveInjector, ApplicationRef, EmbeddedViewRef } from '@angular/core';
 import { PopoverComponent } from './popover.component';
 
 @Injectable({
@@ -14,14 +15,14 @@ export class PopoverService {
   open(viewContainerRef: ViewContainerRef, template: any): PopoverComponent {
     viewContainerRef.clear();
 
-    const popOverFactory = this.resolver.resolveComponentFactory(PopoverComponent)
+    const popOverFactory = this.resolver.resolveComponentFactory(PopoverComponent);
     const injector = ReflectiveInjector.resolveAndCreate([], this.injector);
 
     const popoverRef = viewContainerRef.createComponent(popOverFactory, 0, injector);
     popoverRef.instance.template = template;
-    
+
     setTimeout(() => {
-      popoverRef.instance.onClose.subscribe(() => { 
+      popoverRef.instance.onClose.subscribe(() => {
         this.appRef.detachView(popoverRef.hostView);
         popoverRef.destroy();
       });
@@ -31,26 +32,27 @@ export class PopoverService {
   }
 
   openOnBody(template: any, options: PopoverOptions): PopoverComponent {
-        // Create a component reference from the component 
+        // Create a component reference from the component
       const popoverRef = this.resolver
         .resolveComponentFactory(PopoverComponent)
         .create(this.injector);
 
       popoverRef.instance.template = template;
       popoverRef.instance.options = options;
-    
+
       // Attach component to the appRef so that it's inside the ng component tree
       this.appRef.attachView(popoverRef.hostView);
-      
+
       // Get DOM element from component
       const domElem = (popoverRef.hostView as EmbeddedViewRef<any>)
         .rootNodes[0] as HTMLElement;
-      
+
       // Append DOM element to the body
       document.body.appendChild(domElem);
-      
+      domElem.focus();
+
       setTimeout(() => {
-        popoverRef.instance.onClose.subscribe(() => { 
+        popoverRef.instance.onClose.subscribe(() => {
           this.appRef.detachView(popoverRef.hostView);
           popoverRef.destroy();
         });
@@ -75,7 +77,7 @@ export interface PopoverOptions {
    * When attached to the body, the offset in which the popover should display.
    */
   offset: Offset;
-  
+
 }
 
 export interface Offset {
