52d0e43ab6e411ab251962b68cf790bb0a1648ed
Jonathan Bernard <jbernard@fairwaytech.com>
2019-12-17 10:22:49 -0600

    WIP Mock search filters.

--- webapp/src/app/search/search-filters.resolve.ts
+++ webapp/src/app/search/search-filters.resolve.ts
@@ -11,6 +11,35 @@ export class SearchFiltersResolve implements Resolve<SearchFilters> {
   constructor(private service: SearchService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): SearchFilters | Observable<SearchFilters> | Promise<SearchFilters> {
-    return emptyFilters;
+    const dummyFilters: SearchFilters = {
+      freeText: '',
+      resourceTypes: [
+        { title: 'Instructional', code: 'ir' },
+        { title: 'Professional Learning', code: 'pl' },
+        { title: 'Connections Playlist', code: 'cp' },
+        { title: 'Formative Assessment Strategy', code: 'fas' },
+        { title: 'Accessibility Strategy', code: 'ac' }
+      ],
+      grades: [
+        { title: 'Kindergarten', code: 'k' },
+        { title: '2', code: '2' },
+        { title: '3', code: '3' },
+        { title: '4', code: '4' },
+        { title: '5', code: '5' },
+        { title: '6', code: '6' },
+        { title: '7', code: '7' },
+        { title: '8', code: '8' },
+        { title: 'High School', code: 'hs' }
+      ],
+      subjects: [
+        { title: 'Mathematics', code: 'math' },
+        { title: 'English Language Arts', code: 'ela' }
+      ],
+      claims: [],
+      targets: [],
+      standards: [],
+    };
+
+    return dummyFilters;
   }
 }
