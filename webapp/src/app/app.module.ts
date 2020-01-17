fcefa53750a0d8e49e546d6022e778fec89f0144
Jonathan Bernard <jonathan.bernard@accenture.com>
2020-01-17 13:43:58 -0600

    Fix Okta config in prep for SSO integration: use HTTPS for login URLS.

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -25,7 +25,7 @@ export function initializeApp(appConfig: AppConfig) {
 const oktaConfig = {
   issuer: 'https://smarterbalanced.oktapreview.com/oauth2/auslw2qcsmsUgzsqr0h7',
   clientId: '0oamt7hm566G44ZVm0h7',
-  redirectUri: `http://${window.location.host}/${OKTA_CALLBACK_PATH}`,
+  redirectUri: `https://${window.location.host}/${OKTA_CALLBACK_PATH}`,
   scopes: ['openid', 'profile', 'email', 'digital_library_read']
 };
 
