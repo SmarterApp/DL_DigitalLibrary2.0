c5d486e1c6d0300babb7ae9520b9785a1221c36a
Jonathan Bernard <jonathan.bernard@accenture.com>
2020-01-22 05:25:25 -0600

    Disable notes when the user is not logged in.

--- webapp/src/app/resource/professional/professional-resource.component.ts
+++ webapp/src/app/resource/professional/professional-resource.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, Input } from '@angular/core';
 import { Location } from '@angular/common';
 import { DomSanitizer, Title } from '@angular/platform-browser';
+import { UserService } from 'src/app/data/user/user.service';
 import { ResourceComponent } from '../resource.component';
 import { ProfessionalLearningResource } from '../../data/resource/model/professional-learning.model';
 
@@ -14,7 +15,12 @@ export class ProfessionalResourceComponent extends ResourceComponent {
   @Input()
   resource: ProfessionalLearningResource;
 
-  constructor(cdRef: ChangeDetectorRef, sanitizer: DomSanitizer, titleService: Title, location: Location) {
-    super(cdRef, sanitizer, titleService, location);
-   }
+  constructor(cdRef: ChangeDetectorRef,
+              sanitizer: DomSanitizer,
+              titleService: Title,
+              location: Location,
+              userService: UserService) {
+
+    super(cdRef, sanitizer, titleService, location, userService);
+  }
 }
