e80c909c0dbf69b0172baf01502e548b5a74cf30
Jessica <33644752+jjmuller85@users.noreply.github.com>
2020-07-28 06:35:00 -0700

    Adding structure for the IAIP portal link, but without added criteria dictated by enhancement requirements.

--- webapp/src/app/layout/navigation/navigation.component.ts
+++ webapp/src/app/layout/navigation/navigation.component.ts
@@ -1,10 +1,11 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { OktaAuthService } from '@okta/okta-angular';
 import { Observable } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
+import { AppConfig } from 'src/app/common/config/app.config';
 import { TenantTheme } from 'src/app/data/tenant-theme/tenant-theme.model';
 import { TenantThemeService } from 'src/app/data/tenant-theme/tenant-theme.service';
 import { UserService } from 'src/app/data/user/user.service';
@@ -15,10 +16,11 @@ import { User } from 'src/app/data/user/user.model';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
   hasToken$: Observable<boolean>;
   logo$: Observable<string>;
+  interimItemPortalUrl = '#';
 
   constructor(
     @Inject(APP_BASE_HREF) public baseHref: string,
@@ -35,6 +37,10 @@ export class NavigationComponent {
 
   get user$(): Observable<User> {
     return this.userService.user;
+  }
+
+  ngOnInit() {
+    this.interimItemPortalUrl = AppConfig.settings.interimItemPortalUrl;
   }
 
   login() {
