3ca8f5acb46b9de86cee2de23c86b3935ff6058b
Jim Ruhl <jim.ruhl@accenture.com>
2021-02-26 11:09:27 -0800

    WIP

--- webapp/src/app/data/landing/landing.service.ts
+++ webapp/src/app/data/landing/landing.service.ts
@@ -4,16 +4,26 @@ import { map } from 'rxjs/operators';
 import { DataService } from '../data.service';
 import { Landing } from './model/landing.model';
 
+// TODOJR: will need to remove
+import { HttpClient } from '@angular/common/http';
+import { MockDataService } from '../mock-data.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LandingService {
-  landing: Landing;
-  constructor(private dataService: DataService) { }
+  url: string = 'http://localhost:3000/landing';
+  mockDataService: MockDataService 
+  landings: Observable<Landing>;
 
-  // get = (resourceKey: string): Observable<Landing> => {
-  //  TODOJR
-  // };
 
+  constructor(private dataService: DataService,
+    private http: HttpClient) { }
+
+    
+  get = (resourceKey: string): Observable<Landing> => {
+    this.landings = this.mockDataService.get(this.url, resourceKey)
+    return this.landings[0];
+    }
   
 }
--- webapp/src/app/landing/landing.component.ts
+++ webapp/src/app/landing/landing.component.ts
@@ -10,7 +10,7 @@ import { Landing } from '../data/landing/model/landing.model';
 export class LandingComponent implements OnInit {
   landing: Landing;
   resourceType: string;
-
+ 
   constructor(
     private route: ActivatedRoute,
     private router: Router) { 
@@ -20,7 +20,21 @@ export class LandingComponent implements OnInit {
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     this.resourceType = this.route.snapshot.paramMap.get('resourceType');
+
+    if (this.resourceType === "cp")
+      this.getCP();
     
   }
 
+  getCP()  {
+
+    let landing = {} as Landing;
+
+    landing.id = 1;
+    landing.title = "Interim Connections Playlist";
+    landing.type = "cp";
+      
+    this.landing = landing;
+
+    };
 }
