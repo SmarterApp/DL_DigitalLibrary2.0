5a3c3d934b954bd24581b5c7c596cb50ebe2393a
Jim Ruhl <jim.ruhl@accenture.com>
2021-03-01 11:16:01 -0800

    continue adding to the landing page

--- webapp/src/app/landing/landing.component.ts
+++ webapp/src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Landing } from '../data/landing/model/landing.model';
+import { HowHelp } from '../data/landing/model/howhelp.model' ;
 
 @Component({
   selector: 'sbdl-landing',
@@ -43,6 +44,12 @@ export class LandingComponent implements OnInit {
     landing.titleShort = "Playlist";
     landing.introTitle = "Connect student performance to instructional resources.";
     landing.introText = "Playlist link students' results on interim assessments to ELA and math lessions matched to their knowledge and skills.  Playlists help educators ensure that students receive instruction tailored for their individual performance level - a key to accelerating learning.";
+    landing.howHelp = [ 
+      {id: 1, title: "Support Students",  message: "Use performance progressions to help identify where students are in their learning process and how they can progress to the next level." },
+      {id: 2, title: "Understand the Contest",  message: "Each playlist provides the knowledge and skills students need toachieve within a block of grade-level content." },
+      {id: 3, title: "Plan Instruction",  message: "Use student performance data to inform and plan instrutional next steps." },
+      {id: 3, title: "Take Action",  message: "Use teacher-created instructional resources to support student success." }
+    ];
 
     this.landing = landing;
     };
@@ -57,7 +64,11 @@ export class LandingComponent implements OnInit {
     landing.titleShort = "Instructional";
     landing.introTitle = "Instructional introTitle";
     landing.introText = "Instructional introText";
-      
+    landing.howHelp = [ 
+      {id: 1, title: "Instructional title 1",  message: "Instructional  message 1" },
+      {id: 2, title: "Instructional title 2",  message: "Instructional  message 2" },
+      {id: 3, title: "Instructional title 3",  message: "Instructional  message 3" }
+    ];      
     this.landing = landing;
   };
 
