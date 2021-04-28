6000d08774d4b4f27b7f6d35a106022e83a78e4b
Jim Ruhl <jim.ruhl@accenture.com>
2021-04-28 12:37:29 -0700

    debug code

--- webapp/src/app/common/controls/popover/popover.component.ts
+++ webapp/src/app/common/controls/popover/popover.component.ts
@@ -62,28 +62,38 @@ export class PopoverComponent implements AfterViewInit {
     const offset = this.options.offset;
     if (offset) {
 
+
+
       //TODOJR: cleanup code
       const rect = this.container.nativeElement.getBoundingClientRect();
       const height = rect.bottom - rect.top;
       this.top = this.options.placement === 'top' ? offset.top - height - 28 : offset.top;
       const position = this.options.isScrollable ? 'position: absolute' : 'position: fixed; z-index: 10';
 
-      console.log('this.window.innerHeight:' + this.window.innerHeight);
+      console.log("Popover");
+      console.log("Popover");
+      console.log("Popover");
+      console.log("this.options.offset");
+      console.log(this.options.offset);
+      console.log("rect");
+      console.log(rect);
+      console.log("height: " + height);
+      console.log("this.top: " +  this.top);
+      
+
+      //console.log('this.window.innerHeight:' + this.window.innerHeight);
       //this.top = this.window.innerHeight;
 
-      const toppie = this.top
       setTimeout( () => {
-        this.cssVarStyle = this.sanitizer.bypassSecurityTrustStyle(`${position}; top: ${toppie}px; left: ${offset.left}px`);
-        //this.cssVarStyle = this.sanitizer.bypassSecurityTrustStyle(`position: fixed; z-index: 10; top: ${top}px; left: ${offset.left}px`);
+        this.cssVarStyle = this.sanitizer.bypassSecurityTrustStyle(`${position}; top: ${this.top}px; left: ${offset.left}px`);
         this.isVisible = true;
       }, 0);
       setTimeout(() => {
-        console.log("this.options.offset");
-        console.log(this.options.offset);
-        if (!this.inView(rect, this.options.offset as DOMRect)) {
+        if (!this.inView(rect, this.options.offset)) {
           console.log("scrollIntoView");
-          this.container.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
-          //this.container.nativeElement.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+          //this.container.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+          this.container.nativeElement.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+          //this.container.nativeElement.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'start'});
         }
         this.closeButton.nativeElement.focus();
         console.log("done");
@@ -109,21 +119,35 @@ export class PopoverComponent implements AfterViewInit {
     }
   }
 
-  inView(bounding: DOMRect, offset: DOMRect): boolean {
+  inView(bounding: DOMRect, offset: any): boolean {
     console.log("bounding: DOMRect");
     console.log(bounding);
     console.log("offset");
     console.log(offset);
-    console.log("bounding.top >= 0:" + (bounding.top >= 0)) ;
-    console.log("bounding.left >= 0:" + (bounding.left >= 0)) ;
-    console.log("bounding.bottom <= this.window.innerHeight:" + (bounding.bottom <= this.window.innerHeight));
+    //console.log("bounding.top >= 0:" + (bounding.top >= 0)) ;
+    //console.log("bounding.left >= 0:" + (bounding.left >= 0)) ;
+    //console.log("bounding.bottom <= this.window.innerHeight:" + (bounding.bottom <= this.window.innerHeight));
     console.log("this.window.innerHeight:" + (this.window.innerHeight));
-    console.log("bounding.right <= this.window.innerWidth:" + (bounding.right <= this.window.innerWidth));
+    //console.log("bounding.right <= this.window.innerWidth:" + (bounding.right <= this.window.innerWidth));
     console.log("this.window.innerWidth:" + (this.window.innerWidth));
-    return (bounding.top >= 0 &&
-            bounding.left >= 0 &&
-            bounding.bottom <= this.window.innerHeight &&
-            bounding.right <= this.window.innerWidth);
+
+    //console.log("((offset.left + (bounding.width / 2) >= this.window.innerWidth):" 
+    //+ (offset.left + (bounding.width / 2) >= this.window.innerWidth));
+
+    console.log("Total test:" + ((offset.actualTop - bounding.height >= 0 &&
+      offset.actualLeft - (bounding.width / 2) >= 0 &&
+      offset.actualLeft + (bounding.width / 2) <= this.window.innerWidth)));
+
+
+    console.log("offset.actualTop - bounding.height: " + (offset.actualTop - bounding.height));   
+    console.log("offset.actualLeft - (bounding.width / 2): " + (offset.actualLeft - (bounding.width / 2)));
+    console.log("offset.actualLeft + (bounding.width / 2): " + (offset.actualLeft + (bounding.width / 2)));              
+    return (offset.actualTop - bounding.height >= 0 &&
+            offset.actualLeft - (bounding.width / 2) >= 0 &&
+            offset.actualLeft + (bounding.width / 2) <= this.window.innerWidth);
+            // bounding.left >= 0 &&
+            // bounding.bottom <= this.window.innerHeight &&
+            // bounding.right <= this.window.innerWidth);
   }
 
   close() {
